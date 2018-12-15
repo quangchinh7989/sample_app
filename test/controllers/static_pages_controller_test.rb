3032c60d8e1b59328038dc8c85cbd80e93897142
Quang Chinh <lechinh.sv.ktktcn@gmail.com>
2018-12-15 12:38:04 +0700

    Complet advanced setup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get root" do
-    get static_pages_home_url
+    get '/'
     assert_response :success
   end
 
